bc0cb7f8174d8eeaf6685102ec818484ef9feda9
patrick-wittybunny <patrick.wittybunny@gmail.com>
2019-03-22 17:15:45 +0800

    Update boiler

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
 
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('port', process.env.PORT || 6000);
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on %d', app.get('port'));
+});
+
+app.get('/hello', function(req,res){
+    // console.log(req);
+    console.log(req.body);
+    res.send(200);
 });
 
 
